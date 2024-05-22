5282d51575ab97b36dc553dbe0505bf7ca54ae53
Hardik Gujrathi <gujarathihardik29@gmail.com>
2024-05-23 00:27:17 +0530

    configuring to deploy

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectDB from "./db/dbConfig.js";
 import { userRouter, authRouter, listingRouter } from "./routes/routesIndex.js";
+import path from "path";
 dotenv.config();
 const app = express();
 const port = 3000;
 // Connecting DB
 connectDB();
+const __dirname = path.resolve();
+
 // Starting The Server
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
@@ -20,6 +23,11 @@ app.use("/api/user", userRouter); // User endpoint
 app.use("/api/auth", authRouter); //Auth endpoint
 app.use("/api/list", listingRouter); //Listings endpoint
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 // Middlewares
 // Middleware for handling errors
 app.use((err, req, res, next) => {
